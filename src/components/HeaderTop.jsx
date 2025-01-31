02d3d6c636b89e9d1d934d5f5cc01687e02b9168
Your Name <saurabhbodakhe2004@gmail.com>
2025-01-31 20:49:12 +0530

    removed my account dropdown

--- src/components/HeaderTop.jsx
+++ src/components/HeaderTop.jsx
@@ -22,39 +22,7 @@ export const HeaderTop = () => {
         <div className="flex flex-col sm:flex-row items-end justify-end text-sm py-2">
           
 
-          {/* Right Section */}
-          <div className="flex items-center space-x-4 sm:space-x-6">
-            {/* Account Dropdown */}
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <button className="flex items-center space-x-2 hover:bg-orange-400 hover:text-white px-2 py-1 transition-colors duration-200">
-                  <User className="h-4 w-4" />
-                  <span className="hidden sm:inline">My Account</span>
-                  <ChevronDown className="h-4 w-4" />
-                </button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent align="end" className="w-48">
-                <DropdownMenuItem asChild>
-                  <Link href="/register">
-                    <div className="flex items-center space-x-2">
-                      <User className="h-4 w-4" />
-                      <span>Register</span>
-                    </div>
-                  </Link>
-                </DropdownMenuItem>
-                <DropdownMenuItem asChild>
-                  <Link href="/login">
-                    <div className="flex items-center space-x-2">
-                      <User className="h-4 w-4" />
-                      <span>Login</span>
-                    </div>
-                  </Link>
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-
-            
-          </div>
+         
         </div>
       </div>
     </header>
