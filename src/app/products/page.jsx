1fb39101f6ec27fda7ea796a37cee8ca4660cd3d
Your Name <saurabhbodakhe2004@gmail.com>
2025-02-07 14:48:52 +0530

    add banners

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -3,15 +3,24 @@ import { useSearchParams } from 'next/navigation'
 import React, { useEffect } from 'react'
 import ProductsCards from '../../components/ProductsCards'
 
+
+export default function ProductsByCategoryPage() {
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <ProductsByCategory />
+        </Suspense>
+    );
+}
+
 const ProductsByCategory = () => {
 
     const searchParams = useSearchParams();
     const category = searchParams.get('category');
     const products = searchParams.get('products');
 
-    useEffect(()=>{
-        console.log("category on cat",category)
-        console.log("products on cat",products)
+    useEffect(() => {
+        console.log("category on cat", category)
+        console.log("products on cat", products)
     })
 
 
@@ -22,4 +31,4 @@ const ProductsByCategory = () => {
     )
 }
 
-export default ProductsByCategory
+
