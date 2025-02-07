d8b02f045d85c4c7e63620113cb0ac7f0223b41e
Your Name <saurabhbodakhe2004@gmail.com>
2025-02-07 21:40:54 +0530

    update category card grid

--- src/components/Banners.jsx
+++ src/components/Banners.jsx
@@ -60,7 +60,7 @@ export default function Banners() {
 
   useEffect(() => {
     fetchBanners()
-  })
+  },[])
 
 
 
--- src/components/CategoryCards.jsx
+++ src/components/CategoryCards.jsx
@@ -34,7 +34,7 @@ export default function CategoryCards() {
   return (
     <div className="container mx-auto px-6 sm:px-32 py-8">
       <h2 className="text-2xl font-bold mb-6">COLLECTIONS</h2>
-      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 sm:grid-cols-6 gap-4">
+      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 sm:grid-cols-6 gap-4">
         {categories.map((category, index) => (
           <Card 
             key={index}
